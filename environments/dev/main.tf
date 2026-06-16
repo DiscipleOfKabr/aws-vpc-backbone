f702d70e7a9f49332ad183aa21d37b362ac07835
Alex <kapetsonisalexandros@gmail.com>
2026-06-16 14:51:40 +0300

    Additionn : Implement NAT Gateway and private subnet routing topology

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,32 +1,32 @@
 terraform {
 
 
-    required_version = ">= 1.0"
-    required_providers{
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-        }
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
 
-region = "eu-west-1"
+  region = "eu-west-1"
 
 }
 
 module "dev_network" {
-    source = "../../modules/vpc"
-    vpc_cidr = "10.10.0.0/16"
-    env_name = "dev"
+  source   = "../../modules/vpc"
+  vpc_cidr = "10.10.0.0/16"
+  env_name = "dev"
 
-public_subnet_cidr = "10.10.1.0/24"
-private_subnet_cidr = "10.10.2.0/24"
+  public_subnet_cidr  = "10.10.1.0/24"
+  private_subnet_cidr = "10.10.2.0/24"
 }
 
-output "dev_vpc_id"{
+output "dev_vpc_id" {
 
-    value   = module.dev_network.vpc_id 
-    description = "The ID of the deployed dev vpc"
+  value       = module.dev_network.vpc_id
+  description = "The ID of the deployed dev vpc"
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -75,3 +75,56 @@ resource "aws_route_table_association" "public" {
   subnet_id   = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+
+resource "aws_eip" "nat"{
+
+
+domain ="vpc"
+
+tags = {
+
+  Name = "${var.env_name}-nat-eip"
+}
+
+}
+
+resource "aws_nat_gateway" "main" {
+
+allocation_id = aws_eip.nat.id
+subnet_id     = aws_subnet.public.id 
+
+
+tags = {
+
+
+  Name = "${var.env_name}-nat-gw"
+
+
+  
+}
+depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.backbone.id
+
+  route {
+
+    cidr_block    = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name ="${var.env_name}-private-rt"
+  }
+
+
+}
+
+resource "aws_route_table_association" "private" {
+
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
